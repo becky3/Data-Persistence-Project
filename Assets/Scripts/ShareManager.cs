177d3817b9efba4efd1d64bddf00fd0dc6bd4398
becky <satoru-kibe@rhythmcan.com>
2022-07-18 18:26:05 +0900

    Add Resources

--- Assets/Scripts/ShareManager.cs
+++ Assets/Scripts/ShareManager.cs
@@ -41,7 +41,7 @@ public class ShareManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        LoadBestScore();
+        
     }
 
     // Update is called once per frame
